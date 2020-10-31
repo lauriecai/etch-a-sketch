f37fcb6c232555788563d3c4bffccbf8f44904cf
Laurie Cai <laurie801@gmail.com>
2020-10-31 16:21:49 -0700

    Prompt user for grid size

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-// selector
+// -----SELECTORS-----
 const game = document.querySelector('.game');
 const button = document.querySelector('.button');
+let boardDimension, gridItems;
 
-// variables
-let boardDimension = 16;
+// -----FUNCTIONS-----
+// generate grid item and add to grid
+function generateGridItem() {
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('gridItem');
+    gridContainer.appendChild(gridItem);
+}
+// generate entire grid (16x16 = 256 grid items)
+function generateBoard() {
+    boardDimension = prompt('How many squares do you want per side on your next grid?');
 
+    gridContainer.style.gridTemplateColumns = `repeat(${boardDimension}, auto)`;
+    gridContainer.style.gridTemplateRows = `repeat(${boardDimension}, auto)`;
+    gridContainer.style.display = 'inline-grid';
+    
+    for (let i = 0; i < (Math.pow(boardDimension, 2)); i++) {
+        generateGridItem();
+    }
+
+    // obtain nodelist of all grid items
+    gridItems = document.querySelectorAll('.gridItem');
+
+    // add event listener to each grid item
+    for (let i = 0; i < (Math.pow(boardDimension, 2)); i++) {
+        gridItems[i].addEventListener('mouseover', () => {
+            gridItems[i].classList.add('activated');
+        });
+    }
+}
+// clear board
+function clearBoard() {
+    for (let i = 0; i < (Math.pow(boardDimension, 2)); i++) {
+        gridItems[i].classList.remove('activated');
+        gridContainer.removeChild(gridItems[i]);
+        gridContainer.style.display = 'none';
+    }
+    boardDimension = 0;
+    
+}
+
+// -----FLOW-----
 // create grid div
 const gridContainer = document.createElement('div');
 gridContainer.classList.add('gridContainer');
@@ -12,45 +51,14 @@ gridContainer.classList.add('gridContainer');
 // add grid to game
 game.appendChild(gridContainer);
 
-// generate grid item and add to grid
-function generateGridItem() {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('gridItem');
-    gridContainer.appendChild(gridItem);
-}
-
-// generate entire grid (16x16 = 256 grid items)
-for (let i = 0; i < (Math.pow(boardDimension, 2)); i++) {
-    generateGridItem();
-}
-
-// obtain nodelist of all grid items
-const gridItems = document.querySelectorAll('.gridItem');
-console.log(gridItems[0]);
-
-// add event listener to each grid item
-for (let i = 0; i < 256; i++) {
-    gridItems[i].addEventListener('mouseover', () => {
-        gridItems[i].classList.add('activated');
-    })
-}
-
-// clear board
-function clearBoard() {
-    for (let i = 0; i < 256; i++) {
-        gridItems[i].classList.remove('activated');
-    }
-}
-
-// prompt grid size
-function promptGrid() {
-    boardDimension = prompt('How many squares do you want per side on your next grid?');
-}
+// generate board
+generateBoard();
 
 // button behavior
 button.addEventListener('click', () => {
     clearBoard();
     setTimeout(function() {
-        promptGrid();
-    }, 1000); 
+        generateBoard();
+    }, 1000);
 })
+
