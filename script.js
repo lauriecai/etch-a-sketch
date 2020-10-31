f4b8bdd3acda3170772a523c5a9d4b176e36b733
Laurie Cai <laurie801@gmail.com>
2020-10-31 11:43:58 -0700

    Style grid

--- script.js
+++ script.js
@@ -2,25 +2,20 @@
 const game = document.getElementById('game');
 
 // create grid div
-const grid = document.createElement('div');
-grid.classList.add('gridContainer');
-
-// configure grid
-grid.style.cssText = 'display: grid; grid-template-columns: 16; grid-template-rows: 16;';
+const gridContainer = document.createElement('div');
+gridContainer.classList.add('gridContainer');
 
 // add grid to game
-game.appendChild(grid);
+game.appendChild(gridContainer);
 
 // generate grid item and add to grid
 function generateGrid() {
     const gridItem = document.createElement('div');
     gridItem.classList.add('gridItem');
-    grid.appendChild(gridItem);
+    gridContainer.appendChild(gridItem);
 }
 
 // repeat 16 times
 for (let i = 0; i < 16; i++) {
     generateGrid();
 }
-
-
